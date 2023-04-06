b1f5673f3c6b7e6bdd6f448713f071854ea7b3f1
Pedro <pedro.vsousa89@gmail.com>
2023-04-06 21:29:16 +0100

    Added screenshot component

--- src/test/java/com/example/bddseleniumspring/googletest/GoogleTest.java
+++ src/test/java/com/example/bddseleniumspring/googletest/GoogleTest.java
@@ -2,17 +2,23 @@ package com.example.bddseleniumspring.googletest;
 
 import com.example.bddseleniumspring.SpringBaseTestNGTest;
 import com.example.bddseleniumspring.page.google.GooglePage;
+import com.example.bddseleniumspring.util.ScreenShotUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.io.IOException;
 
 public class GoogleTest extends SpringBaseTestNGTest {
 
     @Autowired
     private GooglePage googlePage;
 
+    @Autowired
+    private ScreenShotUtil screenShotUtil;
+
     @Test
-    public void googleTest(){
+    public void googleTest() throws IOException {
         this.googlePage.goTo();
         this.googlePage.closeCookies();
         Assert.assertTrue(this.googlePage.isAt());
@@ -20,6 +26,7 @@ public class GoogleTest extends SpringBaseTestNGTest {
         this.googlePage.getSearchComponent().search("spring boot");
         Assert.assertTrue(this.googlePage.getSearchResult().isAt());
         Assert.assertTrue(this.googlePage.getSearchResult().getCount() > 2);
+        this.screenShotUtil.takeScreenShot();
     }
 
 }
